69eec3af595d07ac3c4c6bcbd7026409aeda403b
Aditya Painuli <adityapainuli2004@gmail.com>
2023-04-16 12:53:37 +0530

    'set-up-project'

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+
+import Button from '@/components/UI/Button';
+import {FC} from 'react';
+
+
+interface pageProps  {};
+
+const page:FC<pageProps> = ({}) => {
+  return (
+    <div>
+        <Button>Hello world</Button>
+    </div>
+  )
+}
+
+export default page
